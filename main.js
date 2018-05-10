292278df881f5b8e1bdc6a74c0bf4bfaa52603e1
Dan <dansonlinepresence@gmail.com>
2018-05-09 23:14:01 -0400

    cache

--- main.js
+++ main.js
@@ -59,3 +59,14 @@ input.addEventListener('keyup', function(event){
 
 input.focus();
 reset();
+
+this.addEventListener('install', function(event){
+	event.waitUntil(
+		caches.open('v1').then(function(cache){
+			return cache.addAll([
+				'index.html',
+				'main.js',
+			])
+		})
+	)
+})
